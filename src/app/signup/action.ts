974a5cab7bf20f2e565136108361fa182b2f4ce1
Truong Nguyen <trg.mnguyen@gmail.com>
2025-09-07 14:17:27 -0400

    debugging create account

--- src/app/signup/action.ts
+++ src/app/signup/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { createClient } from "../../../supabase/client";
+import { createClient } from "@sb/client";
 
 export type Credentials = {
   email: string;
@@ -17,10 +17,19 @@ export async function signupAction(credentials: Credentials) {
     ...credentials,
     email_confirm: true,
   });
+
+  console.log(result.error)
+
   if (credentials.password !== credentials.confirmPassword)
     return "Passwords do not match";
   if (credentials.password.length < 8)
     return "Password must be at least 8 characters long and include a number and a special character";
   if (!result.data.user) return "Error creating user";
+
+
+  await client.auth.admin.updateUserById(result.data.user?.id, {
+    email_confirm: true,
+  });
+
   redirect("/login");
 }
