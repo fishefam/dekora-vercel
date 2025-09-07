6974d92802f1b5454e7c2042825f0c39252653e3
Truong Nguyen <trg.mnguyen@gmail.com>
2025-09-07 13:20:12 -0400

    experiment radix ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/lib/ui/button";
 import Image from "next/image";
 
 export default async function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Button>CLICK ME NOW</Button>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
