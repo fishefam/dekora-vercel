a9693b5cfcc8987cd5fd218c565e8fe92fb6dbb0
Truong Nguyen <trg.mnguyen@gmail.com>
2025-09-22 22:09:42 -0400

    add new icons

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -18,7 +18,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Shuffle } from "lucide-react";
+import { Shuffle } from "@/components/icons";
 
 export default function Page() {
   return (
